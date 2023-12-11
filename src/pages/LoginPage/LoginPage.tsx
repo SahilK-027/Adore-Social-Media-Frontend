09bc950c5e299b7cad1da2e57cb40851b7f27961
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2023-12-11 19:33:27 +0530

    :bug: Fix invalid import (LoginPage)

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,6 @@
 import GradientEffect from "../../components/GradientEffect/GradientEffect";
 import LoginCard from "../../components/LoginCard/LoginCard";
 import LoginPageBg from "../../components/LoginPageBg/LoginPageBg";
-import "./LoginPage.scss";
 
 const LoginPage = () => {
   return (
